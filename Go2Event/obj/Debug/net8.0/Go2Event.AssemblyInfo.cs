c3d629669ac8c3a662fd518ea10a3c721a5bd608
tanvir-005 <tanvir.24633@gmail.com>
2025-03-27 18:02:11 +0600

    second

--- Go2Event/obj/Debug/net8.0/Go2Event.AssemblyInfo.cs
+++ Go2Event/obj/Debug/net8.0/Go2Event.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Go2Event")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1bd1f2d794f6f07eb3d15bdf7ad83d0d99bf0bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Go2Event")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Go2Event")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
